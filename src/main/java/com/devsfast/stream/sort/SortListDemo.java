3cd31b01da1fa57feb3a905591981bb275956fbb
ryantrinh <coldpalaces@gmail.com>
2021-04-09 01:17:21 -0230

    Added Stream Sort Method

--- src/main/java/com/devsfast/stream/sort/SortListDemo.java
+++ src/main/java/com/devsfast/stream/sort/SortListDemo.java
@@ -6,29 +6,38 @@ import com.devsfast.stream.api.Employee;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class SortListDemo {
     public static void main(String[] args) {
         List<Employee> employees = Database.getEmployees();
 
         //Traditional Method
+
         //ASC
 //        Collections.sort(employees, (o1, o2) -> (int) (o1.getSalary() - o2.getSalary()));
         //DES
 //        Collections.sort(employees, (o1, o2) -> (int) (o2.getSalary() - o1.getSalary()));
 
-        Collections.sort(employees, new MyComparator());
+//        Collections.sort(employees, new MyComparator());
+//        printList(employees);
 
-        printList(employees);
+        //Stream sort method
+
+        employees.stream()
+                .sorted((o1, o2) -> (int) (o1.getSalary() - o2.getSalary()))
+                .forEach(employee -> System.out.println(employee));
+
 
     }
-    public static void printList(List<Employee> employees){
+
+    public static void printList(List<Employee> employees) {
         employees.forEach(employee -> System.out.println(employee));
     }
 
 }
 
-class MyComparator implements Comparator<Employee>{
+class MyComparator implements Comparator<Employee> {
     @Override
     public int compare(Employee o1, Employee o2) {
         return o1.getDepartment().compareTo(o2.getDepartment());
