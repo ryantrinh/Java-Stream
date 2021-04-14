deedc37512a829c8e8eae3c6c44b7ae60cace9d6
ryantrinh <coldpalaces@gmail.com>
2021-04-14 02:00:09 -0230

    Added Flatmap method

--- src/main/java/com/devsfast/stream/mapflatmap/Demo.java
+++ src/main/java/com/devsfast/stream/mapflatmap/Demo.java
@@ -7,9 +7,18 @@ import java.util.stream.Stream;
 public class Demo {
     public static void main(String[] args) {
         List<Customer> customers = CustomerDatabase.getAll();
+
+        //one to one mapping
         List<String> emails = customers.stream()
                 .map(customer -> customer.getEmail())
                 .collect(Collectors.toList());
         System.out.println(emails);
+
+        //one to many mapping
+        List<String> phones = customers.stream()
+                .flatMap(customer -> customer.getPhoneNumbers().stream())
+                .collect(Collectors.toList());
+
+        System.out.println(phones);
     }
 }
